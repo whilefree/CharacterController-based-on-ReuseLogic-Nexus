b31c7916479cc1040ca46465fdc4040849d75820
whilefree <163608341+whilefree@users.noreply.github.com>
2024-03-18 03:36:01 +0330

    Read me edited

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# CharacterController based on ReuseLogic Nexus
- A Character Controller created based on the powerful OOP Solution for Godot 4.2: ReuseLogic Nexus
+CharacterController based on ReuseLogic Nexus
+A Character Controller created based on the powerful OOP Solution for Godot 4.2: ReuseLogic Nexus
+
+Example 001:
+
+This is the example character controller created in the first tutorial in the tutorial series for my Godot 4.2 addon: ReuseLogic Nexus.
+
+Link to video where this example is created: https://youtu.be/Zl3vo9aSKlY
+
+This is the link to my YouTube Playlist where I upload and share my official tutorials for the ReuseLogic Nexus:
+
+https://www.youtube.com/playlist?list=PLfOCVzuzqIWiPY5Rh8GpzP_DyLA3--cvg
+
+Download link for the addon:
+
+https://www.dropbox.com/scl/fi/lxiija70ios12mb3kco9e/reuse_logic_nexus_preview_edition_01.zip?rlkey=d4xboqhofbqi3pkb9nyq0lqjg&dl=0
+
+Support me by watching the videos, subscribing to my channel, reporting any bugs, and sharing your wisdom with me at my YouTube channel:
+
+https://www.youtube.com/@whilefree
+
+Thank you for your support. Have a great Object Orientalization... :)
---
+++ player.gd
@@ -0,0 +1,63 @@
+extends CharacterBody3D
+
+
+@export var _speed = 8.0
+@export var _jump_velocity = 5.0
+@export var _mouse_sensitivity = 0.001
+
+@export_group("Signals")
+@export var IsOnFloorSignal:SignalPickerChild
+@export var IsFallingSignal:SignalPickerChild
+@export var WalkSignal:SignalPickerChild
+@export var JumpSignal:SignalPickerChild
+@export var VelocityResetSignal:SignalPickerChild
+
+@onready var _brain:Brain = $Brain
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if WalkSignal:
+		_brain.connect(WalkSignal.signal_name, walk)
+	if JumpSignal:
+		_brain.connect(JumpSignal.signal_name, jump)
+	if VelocityResetSignal:
+		_brain.connect(VelocityResetSignal.signal_name, velocity_reset)
+
+func velocity_reset(_data):
+	velocity.x = 0.0
+	velocity.z = 0.0
+
+func walk(_data):
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * _speed
+		velocity.z = direction.z * _speed
+
+func jump(_data):
+	velocity.y = _jump_velocity
+
+func _physics_process(delta):
+	if is_on_floor() && IsOnFloorSignal:
+		_brain.emit_signal(IsOnFloorSignal.signal_name, {})
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	if velocity.y < 0 && IsFallingSignal:
+		_brain.emit_signal(IsFallingSignal.signal_name, {})
+	move_and_slide()
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate(Vector3.UP,-event.relative.x * _mouse_sensitivity)
+		#upper limit
+		if event.relative.y < 0:
+			if rotation.x < deg_to_rad(30):
+				rotate(transform.basis.x,-event.relative.y * _mouse_sensitivity)
+		#lower limit
+		if event.relative.y > 0:
+			if rotation.x > deg_to_rad(-30):
+				rotate(transform.basis.x,-event.relative.y * _mouse_sensitivity)
